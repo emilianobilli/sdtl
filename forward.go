5af109f49a0faa961340d00acc99b265b526267b
Emiliano A. Billi <emiliano.billi@gmail.com>
2024-09-30 14:14:49 -0300

    Update forward.go

--- forward.go
+++ forward.go
@@ -9,11 +9,13 @@ func Forward(dst io.Writer, src io.Reader, bufsz int) error {
 	buff := make([]byte, bufsz)
 	for {
 		n, e := src.Read(buff)
+		fmt.Println("Read", e)
 		if e != nil {
 			return e
 		}
 		fmt.Println("Leido", n)
 		n, e = dst.Write(buff[:n])
+		fmt.Println("Write", e)
 		if e != nil {
 			return e
 		}
